32e811009a49a101900a03f9dc2bf70298cd9e4e
Nikhil Mahajan <nik10.mah@gmail.com>
2023-06-22 10:18:36 +0530

    path modified for local

--- src/main/java/com/insonix/klv/utils/IConstants.java
+++ src/main/java/com/insonix/klv/utils/IConstants.java
@@ -64,7 +64,7 @@ public interface IConstants {
 
 	public static final String CHECKSUM = "checksum";
 
-	public static final String KLV_FILE_PATH = "E:\\Projects\\klv_kennedy\\klvs\\output12.klv";
+	public static final String KLV_FILE_PATH = "E:\\Projects\\klvs\\output12.klv";
 	
 	public static final String FFMPEG_EXTRACT_KLV = "ffmpeg -i udp://@127.0.0.1:2000 -map data-re -codec copy -f data \""+IConstants.KLV_FILE_PATH+"\"";
 	
